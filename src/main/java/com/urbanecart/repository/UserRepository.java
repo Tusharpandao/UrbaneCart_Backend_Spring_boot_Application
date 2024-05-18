8d1cd65b36635553694f3241098fa1b5c08bcd86
Tushar Pandao <pandaotushar1@gmail.com>
2024-05-18 22:08:31 +0530

    user find by Email feature added successfully

--- src/main/java/com/urbanecart/repository/UserRepository.java
+++ src/main/java/com/urbanecart/repository/UserRepository.java
@@ -6,4 +6,6 @@ import com.urbanecart.pojo.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+	User findByEmail(String email);
+
 }
--- src/main/java/com/urbanecart/service/UserService.java
+++ src/main/java/com/urbanecart/service/UserService.java
@@ -2,6 +2,7 @@ package com.urbanecart.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.urbanecart.pojo.User;
 import com.urbanecart.repository.UserRepository;
@@ -10,11 +11,17 @@ import com.urbanecart.repository.UserRepository;
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-
+	
 	public User useradd(User user) {
 		
 		return userRepository.save(user);
 	}
 
-	
+	public User findUser(User user) {
+		
+		
+		return userRepository.findByEmail(user.getEmail());
+	}
+
+
 }
