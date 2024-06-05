b7565632704abe4df8d3854f1fbaf7877c100aaf
Tushar Pandao <pandaotushar1@gmail.com>
2024-06-05 12:31:09 +0530

    mapping added

--- src/main/java/com/urbanecart/controller/UserController.java
+++ src/main/java/com/urbanecart/controller/UserController.java
@@ -3,6 +3,7 @@ package com.urbanecart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class UserController {
 	}
 	
 	
-	
+	@GetMapping("/findUser")
 	public ResponseEntity<?> findUser(@RequestBody User user){
 		
 		User pojoUser =userService.findUser(user);
